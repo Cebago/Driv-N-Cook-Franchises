229488d90a1784a6c9c9ca30f84eb9fd06106424
Baptiste DEMARCHE <batmine3@gmail.com>
2020-04-29 21:38:25 +0200

    file to send an email by passing post arguments

--- sendEmail.php
+++ sendEmail.php
@@ -11,21 +11,20 @@ if (count($_POST) == 3
     $server = htmlspecialchars(strtoupper(trim($_POST['server'])));
     $user = $_POST['user'];
     $email = strtolower(trim($_POST['email']));
-    $error = false;
 
+    if (filter_var($email, FILTER_VALIDATE_EMAIL) && $server == "coucou") {
+        $pdo = connectDB();
+        $queryPrepared = $pdo->prepare("SELECT idUser FROM USER WHERE emailAddress = :email");
+        $queryPrepared->execute([":email" => $email]);
+        $result = $queryPrepared->fetchAll(PDO::FETCH_ASSOC);
 
-
-
-
-
-
-
-
-
-
-
-
-
-} else {
-    header('Location: login.php');
+        if ($result[0]["idUser"] == $user) {
+            $queryPrepared = $pdo->prepare("UPDATE USER SET isActivated = true WHERE idUser = :user");
+            $queryPrepared->execute([":user" => $user]);
+            header("HTTP/1.1 200 OK");
+            exit;
+        }
+    }
 }
+header("HTTP/1.1 401 Unauthorized");
+exit;
