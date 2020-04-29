d716bea6132f601592d84e8499442c63c902d2ae
Baptiste DEMARCHE <batmine3@gmail.com>
2020-04-29 22:26:29 +0200

    send email

--- sendEmail.php
+++ sendEmail.php
@@ -19,8 +19,13 @@ if (count($_POST) == 3
         $result = $queryPrepared->fetchAll(PDO::FETCH_ASSOC);
 
         if ($result[0]["idUser"] == $user) {
-            $queryPrepared = $pdo->prepare("UPDATE USER SET isActivated = true WHERE idUser = :user");
-            $queryPrepared->execute([":user" => $user]);
+            $destination = $email;
+            $subject = "Activation de votre compte Where2Go";
+            $header = "FROM: no-reply-inscription@where2go.fr";
+            $link = "https://where2go.fr/isActivated?pseudo=" . urlencode($pseudo) . "&cle=".urlencode($cle);
+            $message = 'Bonjour ' . $pseudo . 'Bienvenue sur Where 2 Go, 
+            Pour activer votre compte, veuillez cliquer sur le lien ci dessous ou copier/coller dans votre navigateur internet.' . $link . '--------------- Ceci est un mail automatique, Merci de ne pas y répondre.';
+            mail($destination, $subject, $message, $header);
             header("HTTP/1.1 200 OK");
             exit;
         }
