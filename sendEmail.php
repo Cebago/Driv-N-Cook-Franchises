7f8a6f8c643b4b951097b9b1e25f883e27507960
Baptiste DEMARCHE <batmine3@gmail.com>
2020-04-30 09:45:27 +0200

    update database and put id in link in email

--- sendEmail.php
+++ sendEmail.php
@@ -17,18 +17,24 @@ if (count($_POST) == 3
         $queryPrepared = $pdo->prepare("SELECT idUser, lastname, firstname FROM USER WHERE emailAddress = :email");
         $queryPrepared->execute([":email" => $email]);
         $result = $queryPrepared->fetchAll(PDO::FETCH_ASSOC);
+        $idUser = $result[0]['idUser'];
         $lastName = $result[0]['lastname'];
         $firstName = $result[0]['firstName'];
 
         if ($result[0]["idUser"] == $user) {
             $cle = createToken($email);
+            $queryPrepared = $pdo->prepare("UPDATE USER SET token = :token WHERE idUser = :user");
+            $queryPrepared->execute([
+                ":token" => $cle,
+                ":user" => $idUser
+            ]);
             $destination = $email;
-            $subject = "Activation de votre compte Drincook";
-            $header = "FROM: client@drivncook.fr";
-            $link = "https://drivncook.fr/isActivated?cle=".urlencode($cle);
+            $subject = "Activation de votre compte Driv'N Cook";
+            $header = "FROM: franchises@drivncook.fr";
+            $link = "https://franchises.drivncook.fr/isActivated?cle=" . urlencode($cle) . "&id=" . urlencode($idUser) ;
             $message = '
 		Bonjour ' . $lastName . ' ' . $firstName . '
-		Bienvenue sur Driv\'n Cook,
+		Bienvenue sur Driv\'N Cook,
  
 		Pour activer votre compte, veuillez cliquer sur le lien ci-dessous ou le copier/coller dans votre navigateur internet.
  		'.$link.'
