ffa61982c1af282701e8ae182d9d2b4fb44e0673
Baptiste DEMARCHE <batmine3@gmail.com>
2020-05-10 15:50:35 +0200

    new format for info

--- functions/myTruckInfo.php
+++ functions/myTruckInfo.php
@@ -11,12 +11,29 @@ $queryPrepared->execute([
     ":user" => $user
 ]);
 $result = $queryPrepared->fetch(PDO::FETCH_ASSOC);
-$queryPrepared = $pdo->prepare("SELECT openDay as weekDay, DATE_FORMAT(startHour, '%H:%i') as startHour, DATE_FORMAT(endHour, '%H:%i') as endHour FROM OPENDAYS WHERE truck = :truck");
+$queryPrepared = $pdo->prepare("SELECT openDay FROM pa2a2drivncook.OPENDAYS WHERE truck = 1 GROUP BY openDay ORDER BY DAYOFWEEK(openDay)");
 $truck = $result["idTruck"];
 $queryPrepared->execute([
     ":truck" => $truck
 ]);
-$open = $queryPrepared->fetchAll(PDO::FETCH_ASSOC);
-$result["opendays"] = $open;
+$day = $queryPrepared->fetchAll(PDO::FETCH_ASSOC);
+$tmp = [];
+for ($i = 0; $i < count($day); $i++) {
+    $queryPrepared = $pdo->prepare("SELECT op1.idOpen, op1.openDay, op1.startHour as morningOpen, op1.endHour morningClose, op2.startHour lunchOpen, op2.endHour lunchClose FROM OPENDAYS as op1 
+    LEFT  JOIN OPENDAYS as op2 ON op1.openDay = op2.openDay 
+    WHERE op1.openDay = :day 
+      AND op1.truck = :truck 
+      AND op1.startHour != op2.startHour 
+      AND op1.endHour != op2.endHour 
+      AND op1.startHour < op2.startHour
+    ORDER BY op1.startHour");
+    $queryPrepared->execute([
+        ":day" => $day[$i]["openDay"] ,
+        ":truck" => $truck
+    ]);
+    $open = $queryPrepared->fetchAll(PDO::FETCH_ASSOC);
+    $tmp = array_merge($tmp, $open);
+}
+$result["opendays"] = $tmp;
 
 echo json_encode($result);
