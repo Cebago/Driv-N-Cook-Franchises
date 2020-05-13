59ea525e52d449a424b6b477446f14b687c65868
Baptiste DEMARCHE <batmine3@gmail.com>
2020-05-13 23:08:18 +0200

    not beautiful match

--- truckInfo.php
+++ truckInfo.php
@@ -9,37 +9,44 @@ include "navbar.php";
             <div class="input-group-prepend">
                 <span class="input-group-text" id="idTruck">Nom du camion</span>
             </div>
-            <input type="text" id="truckName" class="form-control truck" name="truckName" placeholder="Nom" aria-label="truckId" aria-describedby="addon-wrapping" readonly>
+            <input type="text" id="truckName" class="form-control truck" name="truckName" placeholder="Nom"
+                   aria-label="truckId" aria-describedby="addon-wrapping" readonly>
         </div>
         <div class="input-group flex-nowrap mt-2">
             <div class="input-group-prepend">
                 <span class="input-group-text">Marque</span>
             </div>
-            <input type="text" id="truckManufacturers" class="form-control truck" name="truckManufacturers" placeholder="Marque du camion" aria-label="truckId" aria-describedby="addon-wrapping" readonly>
+            <input type="text" id="truckManufacturers" class="form-control truck" name="truckManufacturers"
+                   placeholder="Marque du camion" aria-label="truckId" aria-describedby="addon-wrapping" readonly>
         </div>
         <div class="input-group flex-nowrap mt-2">
             <div class="input-group-prepend">
                 <span class="input-group-text">Modèle</span>
             </div>
-            <input type="text" id="truckModel" class="form-control truck" name="truckModel" placeholder="Modèle du camion" aria-label="truckId" aria-describedby="addon-wrapping" readonly>
+            <input type="text" id="truckModel" class="form-control truck" name="truckModel"
+                   placeholder="Modèle du camion" aria-label="truckId" aria-describedby="addon-wrapping" readonly>
         </div>
         <div class="input-group flex-nowrap mt-2">
             <div class="input-group-prepend">
                 <span class="input-group-text">Plaque d'immatriculation</span>
             </div>
-            <input type="text" id="licensePlate" class="form-control truck" name="licensePlate" placeholder="Plaque d'immatriculation du camion" aria-label="truckId" aria-describedby="addon-wrapping" readonly>
+            <input type="text" id="licensePlate" class="form-control truck" name="licensePlate"
+                   placeholder="Plaque d'immatriculation du camion" aria-label="truckId"
+                   aria-describedby="addon-wrapping" readonly>
         </div>
         <div class="input-group flex-nowrap mt-2 mb-5">
             <div class="input-group-prepend">
                 <span class="input-group-text">Nombre de kilomètres</span>
             </div>
-            <input type="number" id="km" class="form-control truck" name="km" placeholder="Kilomètres parcourus" aria-label="truckId" aria-describedby="addon-wrapping" readonly>
+            <input type="number" id="km" class="form-control truck" name="km" placeholder="Kilomètres parcourus"
+                   aria-label="truckId" aria-describedby="addon-wrapping" readonly>
         </div>
         <div class="w-75">
             <div class="col-md-6 mx-auto" id="schedule"></div>
             <div class="mx-auto col-md-6">
-                <button class="btn btn-dark" type="button" data-toggle="modal" title="Modifier mes horaires d'ouvertures"
-                        data-target="#hourModal" onclick="getOpenDays(2); setTimeout(changeStatus, 500)">
+                <button class="btn btn-dark" type="button" data-toggle="modal"
+                        title="Modifier mes horaires d'ouvertures"
+                        data-target="#hourModal" onclick="displayOpenDays(2); setTimeout(changeStatus, 500)">
                     Modifier mes horaires
                 </button>
             </div>
@@ -48,112 +55,67 @@ include "navbar.php";
 </div>
 
 <div class="modal fade" id="hourModal" tabindex="-1" role="dialog" aria-labelledby="hourModal" aria-hidden="true">
-    <div class="modal-dialog modal-xl" role="document">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="modifyHours">Modifier mes horaires</h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>
-            </div>
-            <div class="modal-body">
-                <?php
-                $day = array(
-                    "Lundi",
-                    "Mardi",
-                    "Mercredi",
-                    "Jeudi",
-                    "Vendredi",
-                    "Samedi",
-                    "Dimanche",
+    <form method="POST" action="./functions/modifyHours.php">
+        <div class="modal-dialog modal-xl" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="modifyHours">Modifier mes horaires</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <?php
+                    $day = array(
+                        "Lundi",
+                        "Mardi",
+                        "Mercredi",
+                        "Jeudi",
+                        "Vendredi",
+                        "Samedi",
+                        "Dimanche",
 
-                );
-                for ($i = 0; $i < count($day); $i++) {
-                ?>
-                    <div class="mb-3">
-                        <h5 class="modal-title"><?php echo $day[$i]; ?></h5>
-                        <div id="parent<?php echo $day[$i]; ?>">
-                            <div class="custom-control custom-switch">
-                                <input type="checkbox" class="custom-control-input" onclick="changeStatus()" name="Check" id="<?php echo $i ?>">
-                                <label class="custom-control-label" for="<?php echo $i ?>">Fermé</label>
-                            </div>
-                            <div class="input-group flex-nowrap mt-1">
-                                <div class="input-group-prepend">
-                                    <span class="input-group-text">Ouverture</span>
+                    );
+                    for ($i = 0; $i < count($day); $i++) {
+                        ?>
+                        <div class="mb-3">
+                            <h5 class="modal-title"><?php echo $day[$i]; ?></h5>
+                            <div id="parent<?php echo $day[$i]; ?>">
+                                <div class="custom-control custom-switch">
+                                    <input type="checkbox" class="custom-control-input" onclick="changeStatus()"
+                                           name="check<?php echo $day[$i] ?>" id="check<?php echo $day[$i] ?>">
+                                    <label class="custom-control-label" for="check<?php echo $day[$i] ?>">Fermé</label>
                                 </div>
-                                <input type="time" class="form-control openHour" name="<?php echo $day[$i] ?>" placeholder="Horaire d'ouverture" aria-describedby="addon-wrapping">
-                            </div>
-                            <div class="input-group flex-nowrap mt-1">
-                                <div class="input-group-prepend">
-                                    <span class="input-group-text">Fermeture</span>
+                                <div class="input-group flex-nowrap mt-1">
+                                    <div class="input-group-prepend">
+                                        <span class="input-group-text">Planning</span>
+                                    </div>
+                                    <input type="text" class="form-control startHour" id="input<?php echo $day[$i]; ?>"
+                                           name="<?php echo $day[$i]; ?>" placeholder="Horaire d'ouverture"
+                                           aria-describedby="addon-wrapping">
                                 </div>
-                                <input type="time" class="form-control closeHour" name="<?php echo $day[$i] ?>" placeholder="Horaire de fermerture" aria-describedby="addon-wrapping">
+                                <small>Respectez le format XX:XX-XX:XX et séparez par un "/" les différentes
+                                    plages</small>
                             </div>
                         </div>
-                        <a onclick="addHour('<?php echo $day[$i] ?>')" href="#"><i class="fas fa-plus"></i>
-                            &nbsp;Ajouter un nouvel horaire
-                        </a>
-                    </div>
-                <?php
-                }
-                ?>
-            </div>
-            <div class="modal-footer">
-                <button class="btn btn-secondary" data-dismiss="modal">Annuler</button>
-                <button class="btn btn-success" data-dismiss="modal" type="button" onclick="">Modifier les horaires</button>
+                        <?php
+                    }
+                    ?>
+                </div>
+                <div class="modal-footer">
+                    <button class="btn btn-secondary" data-dismiss="modal">Annuler</button>
+                    <button class="btn btn-success" type="submit">Modifier les
+                        horaires
+                    </button>
+                </div>
             </div>
         </div>
-    </div>
+    </form>
 </div>
 
 <script>
-
-    function getOpenDays(user) {
-        const request = new XMLHttpRequest();
-        request.onreadystatechange = function() {
-            if (request.readyState === 4 && request.status === 200) {
-                let myJson = JSON.parse(request.responseText);
-                myJson = myJson["opendays"];
-                for (let i = 0; i < myJson.length; i++) {
-                    const div = document.getElementsByClassName("modal-title");
-                    for (let j = 0; j < div.length; j++) {
-                        if (div[j].textContent === myJson[i]["openDay"]) {
-                            let child = div[j].parentElement.childNodes;
-                            for (let count1 = 0; count1 < child.length; count1++) {
-                                if (child[count1].tagName === "DIV") {
-                                    let child2 = child[count1].childNodes;
-                                    for (let count2 = 0; count2 < child2.length; count2++) {
-                                        if (child2[count2].tagName === "DIV") {
-                                            let child3 = child2[count2].childNodes;
-                                            for (let count3 = 0; count3 < child3.length; count3++) {
-                                                if (child3[count3].tagName === "INPUT" && child3[count3].className === "custom-control-input") {
-                                                    let check = child3[count3];
-                                                    check.checked = true;
-                                                }
-                                                if (child3[count3].tagName === "INPUT" && child3[count3].className === "form-control openHour") {
-                                                    let input = child3[count3];
-                                                    input.value = myJson[i]["startHour"];
-                                                } else if (child3[count3].tagName === "INPUT" && child3[count3].className === "form-control closeHour") {
-                                                    let input = child3[count3];
-                                                    input.value = myJson[i]["endHour"];
-                                                }
-                                            }
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        }
-        request.open('GET','functions/myTruckInfo.php?user=' + user);
-        request.send();
-        changeStatus();
-    }
-
     function changeStatus() {
-        const check = document.getElementsByName("Check");
+        const check = document.getElementsByClassName("custom-control-input");
         for (let i = 0; i < check.length; i++) {
             if (check[i].checked === true) {
                 const parent = check[i].parentElement;
@@ -166,10 +128,36 @@ include "navbar.php";
             }
         }
     }
-    
+
+    function displayOpenDays(user) {
+        const request = new XMLHttpRequest();
+        request.onreadystatechange = function () {
+            if (request.readyState === 4 && request.status === 200) {
+                let myJson = JSON.parse(request.responseText);
+                myJson = myJson["opendays"];
+                for (let i = 0; i < myJson.length; i++) {
+                    const day = document.getElementById("input"+myJson[i]["openDay"]);
+                    day.value = "";
+                }
+                for (let i = 0; i < myJson.length; i++) {
+                    const day = document.getElementById("input"+myJson[i]["openDay"]);
+                    if (day.value === "") {
+                        day.value = myJson[i]["startHour"] + " - " + myJson[i]["endHour"];
+                    } else {
+                        day.value += " / " + myJson[i]["startHour"] + " - " + myJson[i]["endHour"];
+                    }
+                    const check = document.getElementById("check" + myJson[i]["openDay"]);
+                    check.checked = true;
+                }
+            }
+        }
+        request.open('GET', 'functions/myTruckInfo.php?user=' + user);
+        request.send();
+    }
+
     function getTruckInfo(user) {
         const request = new XMLHttpRequest();
-        request.onreadystatechange = function(){
+        request.onreadystatechange = function () {
             if (request.readyState === 4 && request.status === 200) {
                 let myJson = JSON.parse(request.responseText);
                 const input = document.getElementsByClassName("truck");
@@ -234,54 +222,11 @@ include "navbar.php";
                 }
             }
         }
-        request.open('GET','functions/myTruckInfo.php?user=' + user);
+        request.open('GET', 'functions/myTruckInfo.php?user=' + user);
         request.send();
     }
 
-    function addHour(thisDay) {
-        const parent = document.getElementById("parent"+thisDay);
-        const pDiv = document.createElement("div");
-        const open = document.createElement("div");
-        open.className = "input-group flex-nowrap mt-1";
-        const openDiv1 = document.createElement("div");
-        openDiv1.className = "input-group-prepend";
-        const openSpan = document.createElement("span");
-        openSpan.className = "input-group-text";
-        openSpan.innerText = "Ouverture";
-        openDiv1.appendChild(openSpan);
-        const openInput = document.createElement("input");
-        openInput.name = thisDay;
-        openInput.type = "time";
-        openInput.className = "form-control openHour";
-        openInput.placeholder = "Horaire d'ouverture";
-        open.appendChild(openDiv1);
-        open.appendChild(openInput);
-
-        const close = document.createElement("div");
-        close.className = "input-group flex-nowrap mt-1";
-        const closeDiv1 = document.createElement("div");
-        closeDiv1.className = "input-group-prepend";
-        const closeSpan = document.createElement("span");
-        closeSpan.className = "input-group-text";
-        closeSpan.innerText = "Fermeture";
-        closeDiv1.appendChild(closeSpan);
-        const closeInput = document.createElement("input");
-        closeInput.name = thisDay;
-        closeInput.type = "time";
-        closeInput.className = "form-control closeHour";
-        closeInput.placeholder = "Horaire de fermeture";
-        close.appendChild(closeDiv1);
-        close.appendChild(closeInput);
-        parent.appendChild(close);
-
-        pDiv.appendChild(open);
-        pDiv.appendChild(close);
-
-        parent.appendChild(pDiv);
-
-    }
-
-    window.onload = function() {
+    window.onload = function () {
         getTruckInfo(2);
     }
 </script>
