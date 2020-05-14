0e5d6aa841196280aac973f0a6c8eef1679bbf17
Baptiste DEMARCHE <batmine3@gmail.com>
2020-05-14 14:14:54 +0200

    update of the preg_match

--- functions/modifyHours.php
+++ functions/modifyHours.php
@@ -13,12 +13,13 @@ $day = array(
 );
 
 $error = false;
-$listOfErrors = "";
 $match = array(
-    0 => "#^((?:2[0-3]|[01][0-9]):[0-5][0-9] - (?:2[0-3]|[01][0-9]):[0-5][0-9])$#",
-    1 => "#^((?:2[0-3]|[01][0-9]):[0-5][0-9] - (?:2[0-3]|[01][0-9]):[0-5][0-9]) \/ ((?:2[0-3]|[01][0-9]):[0-5][0-9] - (?:2[0-3]|[01][0-9]):[0-5][0-9])$#",
-    2 => "#^((?:2[0-3]|[01][0-9]):[0-5][0-9] - (?:2[0-3]|[01][0-9]):[0-5][0-9]) \/ ((?:2[0-3]|[01][0-9]):[0-5][0-9] - (?:2[0-3]|[01][0-9]):[0-5][0-9]) \/ ((?:2[0-3]|[01][0-9]):[0-5][0-9] - (?:2[0-3]|[01][0-9]):[0-5][0-9])$#"
+    0 => "((?:2[0-3]|[01][0-9]):[0-5][0-9] - (?:2[0-3]|[01][0-9]):[0-5][0-9])"
 );
+$listOfErrors = "";
+for ($j = 1; $j < 10; $j++) {
+    $match = array_merge($match, [$j => $match[$j-1] . " \/ " . $match[0]]);
+}
 
 for ($i = 0; $i < count($day); $i++) {
     if (isset($_POST["check" . $day[$i]]) && isset($_POST[$day[$i]]) ) {
@@ -27,11 +28,16 @@ for ($i = 0; $i < count($day); $i++) {
         echo "<br>" . mb_substr_count($thisday, "/") . "</br>";
         echo $thisday;
         echo "<br>" . $match[$number] . "</br>";
-        if (preg_match($match[$number], $thisday)) {
+        if (preg_match("#^" . $match[$number] . "$#", $thisday)) {
             echo $thisday;
         } else {
             $error = true;
             $listOfErrors = "Merci de saisir le bon format horaire pour " . $day[$i];
         }
     }
+}
+if ($error) {
+    echo "Erreurs !!!";
+} else {
+    echo "DATABASE";
 }
