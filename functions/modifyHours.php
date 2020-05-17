01387c1b39ed9ff9a0437c7235493e2fd3d8bdd4
Batmine3 <batmine3@gmail.com>
2020-05-17 12:12:01 +0200

    Easey close with the swithc checkbox

--- functions/modifyHours.php
+++ functions/modifyHours.php
@@ -13,6 +13,7 @@ $day = array(
     "Dimanche",
 );
 
+$newDay = [];
 $error = false;
 $match = array(
     0 => "((?:2[0-3]|[01][0-9]):[0-5][0-9] - (?:2[0-3]|[01][0-9]):[0-5][0-9])"
@@ -29,23 +30,18 @@ for ($i = 0; $i < count($day); $i++) {
         if ( !preg_match("#^" . $match[$number] . "$#", $thisday)) {
             $error = true;
             $listOfErrors[] = "Merci de saisir le bon format horaire pour " . $day[$i];
+        } else {
+            $thisday = preg_replace("# - #", " / ", $thisday);
+            $newDay = array_merge($newDay, [$day[$i] => preg_split("# / #", $thisday)]);
+            for ($pos = 0; $pos < count($newDay[$day[$i]]) - 1; $pos++) {
+                if ($newDay[$day[$i]][$pos] > $newDay[$day[$i]][$pos + 1]) {
+                    $error = true;
+                    $listOfErrors[] = "Il n'est pas possible que vous ouvriez une deuxième fois avant d'avoir fermé.";
+                }
+            }
         }
     }
 }
-$newDay = [];
-for ($i = 0; $i < count($day); $i++) {
-    $thisday = $_POST[$day[$i]];
-    $thisday = preg_replace("# - #", " / ", $thisday);
-    $newDay = array_merge($newDay, [$day[$i] => preg_split("# / #", $thisday)]);
-    for ($pos = 0; $pos < count($newDay[$day[$i]]) - 1; $pos++) {
-        if ($newDay[$day[$i]][$pos] > $newDay[$day[$i]][$pos + 1]) {
-            $error = true;
-            $listOfErrors[] = "Il n'est pas possible que vous ouvriez une deuxième fois avant d'avoir fermé.";
-        }
-    }
-
-}
-
 if ($error) {
     unset($_POST);
     $_SESSION["errors"] = $listOfErrors;
@@ -67,14 +63,16 @@ if ($error) {
     //$user = $_SESSION["user"];
     $queryPrepared = $pdo->prepare("INSERT INTO OPENDAYS (openDay, startHour, endHour, truck) VALUES (:day, :start, :end, :truck)");
     for ($i = 0; $i < count($day); $i++) {
-        for ($pos = 0; $pos < count($newDay[$day[$i]]); $pos += 2) {
-            if (count($newDay[$day[$i]]) > 1) {
-                $queryPrepared->execute([
-                    ":day" => $day[$i],
-                    ":start" => $newDay[$day[$i]][$pos],
-                    ":end" => $newDay[$day[$i]][$pos + 1],
-                    ":truck" => $truck
-                ]);
+        if (isset($newDay[$day[$i]])) {
+            for ($pos = 0; $pos < count($newDay[$day[$i]]); $pos += 2) {
+                if (count($newDay[$day[$i]]) > 1) {
+                    $queryPrepared->execute([
+                        ":day" => $day[$i],
+                        ":start" => $newDay[$day[$i]][$pos],
+                        ":end" => $newDay[$day[$i]][$pos + 1],
+                        ":truck" => $truck
+                    ]);
+                }
             }
         }
     }
