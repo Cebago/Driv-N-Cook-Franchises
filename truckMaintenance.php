adec601bcef74e1a5877d5c2ddd1048a903299d0
Batmine3 <batmine3@gmail.com>
2020-05-25 18:03:50 +0200

    new modal to make truck available

--- truckMaintenance.php
+++ truckMaintenance.php
@@ -41,18 +41,17 @@ if (count($maintenance) > 0) {
         ]);
         $result = $queryPrepared->fetch(PDO::FETCH_ASSOC);
         if (empty($result)) {
-            echo "<p class='text-success'><i class='fas fa-check'></i>&nbsp;Camion opérationnel</p>";
+            echo "<p class='text-success mb-5'><i class='fas fa-check'></i>&nbsp;Camion opérationnel</p>";
         } else {
-            echo "<p class='text-danger'><i class='fas fa-times'></i>&nbsp;Camion Indisponible</p>";
+            echo "<p class='text-danger mb-5'><i class='fas fa-times'></i>&nbsp;Camion Indisponible</p>";
             if ($result["status"] == 10) {
-                echo "<button class='btn btn-dark mb-5 mr-5 ml-5'>Rendre le camion disponible</button>";
+                echo "<button class='btn btn-dark mb-5 mr-5 ml-5' data-toggle='modal' data-target='#validModal'>Rendre le camion disponible</button>";
             }
         }
         ?>
     </div>
     <div class="col-md-5">
-        <button class="btn btn-danger mb-5 mr-5 ml-5" data-toggle="modal" data-target="#incident">Enregistrer un incident</button>
-        <button class="btn btn-warning mb-5 mr-5 ml-5">Enregistrer une maintenance</button>
+        <button class="btn btn-danger mb-5 mr-5 ml-5" data-toggle="modal" data-target="#incident">Enregistrer une maintenance/incident</button>
     </div>
     <div class="accordion" id="maintenanceCollapse">
         <?php
@@ -61,7 +60,7 @@ if (count($maintenance) > 0) {
                 <div class="card-header" id="<?php echo 'header' . $i ?>">
                     <h2 class="mb-0">
                         <button class="btn btn-link btn-block text-left" type="button" data-toggle="collapse" data-target="#<?php echo 'collapse' . ($i + 1) ?>" aria-expanded="true" aria-controls="<?php echo 'collapse' . $i ?>">
-                            <?php echo $i + 1  . " - " . $maintenance[$i]["maintenanceName"] . " - " . $maintenance[$i]["maintenanceDate"] ?>
+                            <?php echo $maintenance[$i]["maintenanceName"] . " - " . $maintenance[$i]["maintenanceDate"] ?>
                         </button>
                     </h2>
                 </div>
@@ -134,11 +133,32 @@ if (count($maintenance) > 0) {
                     </div>
                     <div class="modal-footer">
                         <button type="button" class="btn btn-secondary" data-dismiss="modal">Annuler</button>
-                        <button type="submit" class="btn btn-danger">Enregistrer l'incident</button>
+                        <button type="submit" class="btn btn-danger">Enregistrer</button>
                     </div>
                 </div>
             </div>
         </form>
     </div>
+
+    <div class="modal fade" id="validModal" data-backdrop="static" data-keyboard="false" tabindex="-1" role="dialog" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="staticBackdropLabel">Modal title</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    Etes-vous sur de rendre le camion de nouveau disponible ?
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Non</button>
+                    <button type="button" class="btn btn-primary">Oui</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
     <?php include "footer.php" ?>
 </body>
