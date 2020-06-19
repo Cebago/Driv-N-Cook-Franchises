7c2ab3a67d2dd76c40e5338285e9e031768127fd
Batmine3 <batmine3@gmail.com>
2020-06-19 15:51:14 +0200

    update payment

--- payment.php
+++ payment.php
@@ -32,14 +32,17 @@
                                     <div class="form-group"><label for="cardNumber">
                                             <h6>Card number</h6>
                                         </label>
-                                        <div class="input-group"><input type="text" name="cardNumber"
+                                        <div class="input-group">
+                                            <input type="text" name="cardNumber"
                                                                         placeholder="Valid card number"
                                                                         class="form-control " required>
-                                            <div class="input-group-append"><span
-                                                        class="input-group-text text-muted"> <i
-                                                            class="fab fa-cc-visa mx-1"></i> <i
-                                                            class="fab fa-cc-mastercard mx-1"></i> <i
-                                                            class="fab fa-cc-amex mx-1"></i> </span></div>
+                                            <div class="input-group-append">
+                                                <span class="input-group-text text-muted">
+                                                    <i class="fab fa-cc-visa mx-1"></i>
+                                                    <i class="fab fa-cc-mastercard mx-1"></i>
+                                                    <i class="fab fa-cc-amex mx-1"></i>
+                                                </span>
+                                            </div>
                                         </div>
                                     </div>
                                     <div class="row">
@@ -47,17 +50,21 @@
                                             <div class="form-group"><label><span class="hidden-xs">
                                                     <h6>Expiration Date</h6>
                                                 </span></label>
-                                                <div class="input-group"><input type="number" placeholder="MM" name=""
-                                                                                class="form-control" required> <input
-                                                            type="number" placeholder="YY" name="" class="form-control"
-                                                            required></div>
+                                                <div class="input-group">
+                                                    <input type="number" placeholder="MM" name="" class="form-control" required>
+                                                    <input type="number" placeholder="YY" name="" class="form-control"
+                                                            required>
+                                                </div>
                                             </div>
                                         </div>
                                         <div class="col-sm-4">
-                                            <div class="form-group mb-4"><label data-toggle="tooltip"
-                                                                                title="Three digit CV code on the back of your card">
-                                                    <h6>CVV <i class="fa fa-question-circle d-inline"></i></h6>
-                                                </label> <input type="text" required class="form-control"></div>
+                                            <div class="form-group mb-4">
+                                                <label data-toggle="tooltip" title="Three digit CV code on the back of your card">
+                                                    <h6>CVV <i class="fa fa-question-circle d-inline"></i>
+                                                    </h6>
+                                                </label>
+                                                <input type="text" required class="form-control">
+                                            </div>
                                         </div>
                                     </div>
                                     <div class="card-footer">
@@ -70,10 +77,16 @@
                         <!-- Paypal info -->
                         <div id="paypal" class="tab-pane fade pt-4">
                             <h6 class="pb-2">Select your paypal account type</h6>
-                            <div class="form-group "><label class="radio-inline"> <input type="radio" name="optradio"
-                                                                                         checked> Domestic </label>
-                                <label class="radio-inline"> <input type="radio" name="optradio" class="ml-5">International
-                                </label></div>
+                            <div class="form-group ">
+                                <label class="radio-inline">
+                                    <input type="radio" name="optradio" checked>
+                                    Domestic
+                                </label>
+                                <label class="radio-inline">
+                                    <input type="radio" name="optradio" class="ml-5">
+                                    International
+                                </label>
+                            </div>
                             <p>
                                 <button type="button" class="btn btn-primary "><i class="fab fa-paypal mr-2"></i> Log
                                     into my Paypal
@@ -82,30 +95,7 @@
                             <p class="text-muted"> Note: After clicking on the button, you will be directed to a secure
                                 gateway for payment. After completing the payment process, you will be redirected back
                                 to the website to view details of your order. </p>
-                        </div> <!-- End -->
-                        <!-- bank transfer info -->
-                        <!--                    <div id="net-banking" class="tab-pane fade pt-3">-->
-                        <!--                        <div class="form-group "> <label for="Select Your Bank">-->
-                        <!--                                <h6>Select your Bank</h6>-->
-                        <!--                            </label> <select class="form-control" id="ccmonth">-->
-                        <!--                                <option value="" selected disabled>--Please select your Bank--</option>-->
-                        <!--                                <option>Bank 1</option>-->
-                        <!--                                <option>Bank 2</option>-->
-                        <!--                                <option>Bank 3</option>-->
-                        <!--                                <option>Bank 4</option>-->
-                        <!--                                <option>Bank 5</option>-->
-                        <!--                                <option>Bank 6</option>-->
-                        <!--                                <option>Bank 7</option>-->
-                        <!--                                <option>Bank 8</option>-->
-                        <!--                                <option>Bank 9</option>-->
-                        <!--                                <option>Bank 10</option>-->
-                        <!--                            </select> </div>-->
-                        <!--                        <div class="form-group">-->
-                        <!--                            <p> <button type="button" class="btn btn-primary "><i class="fas fa-mobile-alt mr-2"></i> Proceed Pyment</button> </p>-->
-                        <!--                        </div>-->
-                        <!--                        <p class="text-muted">Note: After clicking on the button, you will be directed to a secure gateway for payment. After completing the payment process, you will be redirected back to the website to view details of your order. </p>-->
-                        <!--                    </div> <!-- End -->
-                        <!-- End -->
+                        </div>
                     </div>
                 </div>
             </div>
