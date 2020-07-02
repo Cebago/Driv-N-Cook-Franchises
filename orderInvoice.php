4463bc603661964e4dd64324688ec6a3733db3c2
Batmine3 <batmine3@gmail.com>
2020-07-02 22:39:29 +0200

    finish with upload of files

--- orderInvoice.php
+++ orderInvoice.php
@@ -16,7 +16,9 @@ $result = $queryPrepared->fetchAll(PDO::FETCH_ASSOC);
 
 ?>
 
-<?php foreach ($result as $value) { ?>
+<?php
+    include "navbar.php";
+    foreach ($result as $value) { ?>
     <div class="card">
         <div class="card-body">
 
@@ -37,17 +39,15 @@ $result = $queryPrepared->fetchAll(PDO::FETCH_ASSOC);
                         <label for="inputFranchisee" class="col-sm-2 col-form-label">Franchisé</label>
                         <div class="col-sm-10">
                             <input type="text" name="franchisee" id="franchisee" class="form-control"
-                                   placeholder="<?php echo $value['lastname'] ?> <?php echo $value['firstname'] ?>"
-                                   value="<?php echo (isset($_SESSION["inputErrors"]))
-                                       ? $_SESSION["inputErrors"]["franchisee"]
-                                       : ""; ?>" disabled>
+                                   placeholder="<?php echo $value['lastname'] . " " . $value['firstname'] ?>"
+                                   value="<?php echo $value['lastname'] . " " . $value['firstname'] ?>" disabled>
                         </div>
                     </div>
                     <div class="form-group row">
                         <label for="inputPrice" class="col-sm-2 col-form-label">Prix de la facture</label>
                         <div class="col-sm-10">
                             <div class="input-group-append col-md-3">
-                                <input type="number" name="price" id="price" class="form-control"
+                                <input type="number" name="price" required id="price" class="form-control"
                                        aria-label="Amount (to the nearest dollar)"
                                        value="<?php echo (isset($_SESSION["inputErrors"]))
                                            ? $_SESSION["inputErrors"]["price"]
@@ -59,7 +59,7 @@ $result = $queryPrepared->fetchAll(PDO::FETCH_ASSOC);
                     <div class="form-group row">
                         <label for="inputFranchisee" class="col-sm-2 col-form-label">Date de la facturation</label>
                         <div class="col-sm-10  col-md-3">
-                            <input type="date" name="date" id="date" class="form-control" placeholder="jj/mm/aaaa"
+                            <input type="date" name="date" required id="date" class="form-control" placeholder="jj/mm/aaaa"
                                    value="<?php echo (isset($_SESSION["inputErrors"]))
                                        ? $_SESSION["inputErrors"]["date"]
                                        : ""; ?>">
@@ -68,10 +68,7 @@ $result = $queryPrepared->fetchAll(PDO::FETCH_ASSOC);
                     <div class="form-group row">
                         <label for="exampleFormControlFile1">Choisir la facture</label>
                         <div class="col-sm-10">
-                            <input type="file" name="invoice" id="invoice" class="form-control-file"
-                                   value="<?php echo (isset($_SESSION["inputErrors"]))
-                                       ? $_SESSION["inputErrors"]["invoice"]
-                                       : ""; ?>">
+                            <input type="file" required name="invoice" id="invoice" class="form-control-file">
                         </div>
                     </div>
                     <div class="form-group row">
