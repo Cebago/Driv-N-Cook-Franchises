1161017a71f9bc3d7670f383133f4efe051feaa6
Batmine3 <batmine3@gmail.com>
2020-07-05 19:37:28 +0200

    add holidays button

--- home.php
+++ home.php
@@ -11,12 +11,8 @@ if (isActivated() && isFranchisee()) {
         <section class="jumbotron text-center">
             <div class="container">
                 <h1>Accueil franchisés</h1>
-                <p class="lead text-muted">Something short and leading about the collection below—its contents, the
-                    creator,
-                    etc. Make it short and sweet, but not too short so folks don’t simply skip over it entirely.</p>
-                <p>
-                    <a href="#" class="btn btn-primary my-2">Main call to action</a>
-                    <a href="#" class="btn btn-secondary my-2">Secondary action</a>
+                <p id="">
+                    <button class="btn btn-primary my-2">Fermer le camion pour cause de congés</button>
                 </p>
             </div>
         </section>
