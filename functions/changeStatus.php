da0710fd187d53995690589c50fcbd73e49c4e78
Batmine3 <batmine3@gmail.com>
2020-07-05 14:53:14 +0200

    change status

--- functions/changeStatus.php
+++ functions/changeStatus.php
@@ -1,1 +1,20 @@
 <?php
+session_start();
+require "conf.inc.php";
+require "functions.php";
+
+if (isset($_GET["order"], $_GET["status"])) {
+    $order = $_GET["order"];
+    $status = $_GET["status"];
+
+    $pdo = connectDB();
+    $queryPrepared = $pdo->prepare("UPDATE ORDERSTATUS SET status = :status WHERE orders = :order AND status = 27");
+    $queryPrepared->execute([
+        ":status" => $status,
+        ":order" => $order
+    ]);
+
+
+} else {
+    echo "Le statut n'a pas pu être modifié !!";
+}
